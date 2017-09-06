27a7059dd4f627a87af921666dc719e2afab3482
alibenmussa <alibenmussa@gmail.com>
2017-09-06 17:54:42 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){  
+    (function($) {
+        $.fn.clickToggle = function(func1, func2) {
+            var funcs = [func1, func2];
+            this.data('toggleclicked', 0);
+            this.click(function() {
+                var data = $(this).data();
+                var tc = data.toggleclicked;
+                $.proxy(funcs[tc], this)();
+                data.toggleclicked = (tc + 1) % 2;
+            });
+            return this;
+        };
+    }(jQuery));
+    $('.dropdown .click').clickToggle(function() {
+        $('.dropdown ul').animate({left: "-150px"}, 200, function() {
+            $('.dropdown button i').removeClass('fa-bars').addClass('fa-times');
+            $('.dropdown ul').animate({left: "0px"}, 320);
+            $('.dropdown .click').animate({left: "150px"}, 320);
+            $('.dropdown .click').css({position: "fixed", top: "70px"});
+        });
+    }, function() {
+        
+        $('.dropdown .click').animate({left: "0px"}, 320);
+        $('.dropdown ul').animate({left: "-150px"}, 320, function() {
+            $('.dropdown button i').removeClass('fa-times').addClass('fa-bars');
+            $('.dropdown ul').animate({left: "-215px"}, 200);
+            $('.dropdown .click').css({position: "relative", top: "40px"});
+        });
+    });
+    $('.footer-2 button').on("click",function() {
+        $('html, body').animate({ scrollTop: 0 }, 'slow', function () {
+        });
+    });
+});
